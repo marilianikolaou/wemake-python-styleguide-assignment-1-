6ffedc31dc7dddd719afca116d3a79c1e9596799
marilianikolaou <marilianiko@gmail.com>
2024-06-09 12:46:13 +0200

    branch 1 coverage

--- tests/test_violations/test_codes.py
+++ tests/test_violations/test_codes.py
@@ -3,7 +3,6 @@ import atexit
 
 coverage_data = {
     "branch_1": 0,
-    "branch_2": 0,
 }
 
 def test_all_unique_violation_codes(all_violations):
@@ -27,13 +26,12 @@ def test_all_unique_violation_messages(all_violations):
 
 def test_all_violations_correct_numbers(all_module_violations):
     """Ensures that all violations has correct violation code numbers."""
+    coverage_data["branch_1"] += 1
     assert len(all_module_violations) == 7
 
     for index, module in enumerate(all_module_violations.keys()):
         code_number = index * 100
         for violation_class in all_module_violations[module]:
-            if code_number <= violation_class.code <= code_number + 100 - 1:  # Branch 1
-                coverage_data["branch_1"] += 1
             assert (
                 code_number <= violation_class.code <= code_number + 100 - 1
             ), violation_class.__qualname__
@@ -45,8 +43,6 @@ def test_violations_start_zero(all_module_violations):
             violation_class.code
             for violation_class in all_module_violations[module]
         )
-        if starting_code == index * 100:  # Branch 2
-            coverage_data["branch_2"] += 1
         assert starting_code == index * 100
 
 def test_no_holes(all_violation_codes):
@@ -61,7 +57,6 @@ def test_no_holes(all_violation_codes):
 
 def print_code_coverage():
     print(f"'test_all_violations_correct_numbers' function has executed branch_1: {coverage_data['branch_1']} times")
-    print(f"'test_violations_start_zero' function has executed branch_2: {coverage_data['branch_2']} times")
 
 atexit.register(print_code_coverage)
 
